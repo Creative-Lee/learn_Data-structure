86f6a01f0405394bf0577684d64402b3c072b780
Dohyeon Lee <dlehgus1110@naver.com>
2022-03-11 23:09:26 +0900

    트리 구현중

---
+++ 직접구현/tree.js
@@ -0,0 +1,16 @@
+class Node{
+	constructor(value){
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class Tree{
+	contructor(){
+		this.root = null;
+		this.leaf = null;
+		this.size = 0;
+		this.height = 0;		
+	}
+}
